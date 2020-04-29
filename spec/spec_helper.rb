52acac1939fdb2806a37e7638782c63796b1ff11
github username <github email address>
2020-04-29 09:45:25 -0400

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,23 +1,13 @@
 ENV["ACTIVE_RECORD_ENV"] = "test"
 
 require_relative '../config/environment'
-require 'rake'
-load './Rakefile'
 
 RSpec.configure do |config|
 
-  config.run_all_when_everything_filtered = true
-  config.filter_run :focus
-
   config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
+    clear_db
+    system("rake db:migrate")
+    DB.tables
   end
 
 end
